7177849d79845e924eaf3a58cf9e9947edc9b2cc
Touhid7051 <touhidul15-7051@diu.edu.bd>
2021-02-17 15:14:39 +0600

    RD-21 Model Created

---
+++ Website/admin.py
@@ -0,0 +1,17 @@
+from django.contrib import admin
+from Website.models import Contact_Info,Slide_Images,Brands,Topbar_footer,Company_Overview,Products,Board_of_directors,Our_partner,subscribe,NewsandEvent,About_us,Contact
+
+# Register your models here.
+
+admin.site.register(Contact_Info)
+admin.site.register(Slide_Images)
+admin.site.register(Brands)
+admin.site.register(Topbar_footer)
+admin.site.register(Company_Overview)
+admin.site.register(Products)
+admin.site.register(Board_of_directors)
+admin.site.register(Our_partner)
+admin.site.register(subscribe)
+admin.site.register(NewsandEvent)
+admin.site.register(About_us)
+admin.site.register(Contact)
