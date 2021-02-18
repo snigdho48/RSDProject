ff76661e5f73a0703c5fba4006914537d1b1ee4a
snigdho48 <www.atik48@gmail.com>
2021-02-18 16:57:15 +0600

    RD-10 templates Spiting

---
+++ Website/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from . import views
+
+
+urlpatterns = [
+
+        path('',views.home,name='home'),
+        path('event_details/',views.event_details,name='event_details'),
+        path('news/',views.News,name='news'),
+        path('products/',views.products,name='products'),
+        path('products_details/',views.products_details,name='products_details'),
+        path('about/',views.about,name='about'),
+        path('contact/',views.contact,name='contact'),
+        path('partnerships/',views.partnerships,name='partnerships'),
+        path('services/',views.services,name='services'),
+    ]
