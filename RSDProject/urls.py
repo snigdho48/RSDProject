ab8fcbc975ec498c1cd906e88ba29e605776ced5
Touhid7051 <touhidul15-7051@diu.edu.bd>
2021-02-23 17:09:31 +0600

    RD-33 Admin Title Change

--- RSDProject/urls.py
+++ RSDProject/urls.py
@@ -3,6 +3,10 @@ from django.urls import path ,include
 from django.conf import settings
 from django.conf.urls.static import static
 
+admin.site.site_header="RS Meena Admin Panel"
+admin.site.site_title="Welcome To RS Meena Admin Portal!"
+admin.site.index_title="Welcome To RS Meena Portal"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('Website.urls'))
