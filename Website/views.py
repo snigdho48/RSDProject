49e52ce9b4561970b9c3cc2fe016c230e91b101f
snigdho48 <atiquzzaman15-10650@diu.edu.bd>
2021-02-27 15:22:21 +0600

    RD-31 revarsed product list view and fix some bug

--- Website/views.py
+++ Website/views.py
@@ -3,65 +3,68 @@ from Website.models import Contact_Info, Products, Brands, Board_of_directors, N
 from django.shortcuts import render
 from django.views import generic
 
+
 def home(request):
     data = Contact_Info.objects.all()
     product = Products.objects.all()
+    posts = reversed(NewsandEvent.objects.all())
     brands = Brands.objects.all()
     board_of_directors = Board_of_directors.objects.all()
     context = {'data': data,
                'products': product,
                'brands': brands,
+               'posts': posts,
                'board_of_directors': board_of_directors}
     return render(request, 'index.html', context)
 
 
 def about(request):
     data = Contact_Info.objects.all()
-    context = {'data': data,}
-    return render(request, 'about.html',context)
-
-
+    context = {'data': data, }
+    return render(request, 'about.html', context)
 
 
 def NewsList(request):
-    posts = NewsandEvent.objects.all()
+    posts = reversed(NewsandEvent.objects.all())
+    data = Contact_Info.objects.all()
+    context = {'data': data,
+               'posts': posts}
 
-    return render(request, 'News_and_events.html', {'posts': posts})
+    return render(request, 'News_and_events.html', context)
+
 
 def NewsDetail(request, slug):
     post = NewsandEvent.objects.get(slug=slug)
-    return render(request, 'Event_details.html', {'post': post})
-
+    data = Contact_Info.objects.all()
+    context = {'data': data,
+               'post': post}
+    return render(request, 'Event_details.html', context)
 
 
 def partnerships(request):
     data = Contact_Info.objects.all()
-    context = {'data': data,}
-    return render(request, 'partnerships.html',context)
+    context = {'data': data, }
+    return render(request, 'partnerships.html', context)
 
 
 def products(request):
-    data = Products.objects.all()
-    context = {'data': data,}
-    return render(request, 'products.html',context)
-
-
-def products_details(request):
     data = Contact_Info.objects.all()
-    context = {'data': data,}
-    return render(request, 'products_details.html',context)
+    product = Products.objects.all()
+    context = {'data': data,
+               'product': product}
+    return render(request, 'products.html', context)
 
 
 def event_details(request):
     data = Contact_Info.objects.all()
-    context = {'data': data,}
-    return render(request, 'Event_details.html',context)
+    context = {'data': data, }
+    return render(request, 'Event_details.html', context)
 
 
 def contact(request):
     data = Contact_Info.objects.all()
-    context = {'data': data,}
-    return render(request, 'contact.html',context)
+    context = {'data': data, }
+    return render(request, 'contact.html', context)
 
 
 def handler404(request, exception=None):
@@ -69,22 +72,22 @@ def handler404(request, exception=None):
 
 
 def brands(request):
-    data = Brands.objects.all()
-    context = {'data': data,}
-    return render(request, 'brands.html',context)
+    data = Contact_Info.objects.all()
+    brands = Brands.objects.all()
+    context = {'data': data,
+               'brands': brands}
+    return render(request, 'brands.html', context)
 
 
 def career(request):
     data = Contact_Info.objects.all()
-    context={'data': data,}
-    return render(request, 'career.html',context)
-
-
+    context = {'data': data, }
+    return render(request, 'career.html', context)
 
 
 def catalog(request):
     data = Contact_Info.objects.all()
     product = Products.objects.all()
-    context={'products': product,
-             'data':data}
-    return render(request,'catalog.html',context)
+    context = {'products': product,
+               'data': data}
+    return render(request, 'catalog.html', context)
