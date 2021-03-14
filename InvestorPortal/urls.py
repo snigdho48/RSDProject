5bfcde88c33ac4c2bf029c659063d8510a60f693
Touhid7051 <touhidul15-7051@diu.edu.bd>
2021-03-14 15:19:00 +0600

    RD-56 Authentication of Investor Portal

--- InvestorPortal/urls.py
+++ InvestorPortal/urls.py
@@ -13,6 +13,9 @@ urlpatterns = [
         path('issue/', views.issue, name='issue'),
         path('buy_req/', views.request_buy, name='request_buy'),
         path('withdraw_req/', views.request_withdraw, name='request_withdraw'),
+        path('login/', views.loginPage , name='login'),
+        path('logout/', views.logutPage , name='logout'),
+        
 ]
 
 # handler404 = Website.views.error_404
--- InvestorPortal/views.py
+++ InvestorPortal/views.py
@@ -1,31 +1,38 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate , login ,logout
 from .models import *
 
 
 # Create your views here.
 
-
+@login_required(login_url = 'login')
 def p_home(request):
     return render(request, 'investor/index.html')
 
 
+@login_required(login_url = 'login')
 def portfolio(request):
     return render(request, 'investor/portfolio.html')
 
-
+@login_required(login_url = 'login')
 def withdraw(request):
     return render(request, 'investor/withdraw.html')
 
-
+@login_required(login_url = 'login')
 def buy_more(request):
     return render(request, 'investor/buy_more.html')
 
-
+@login_required(login_url = 'login')
 def issue(request):
     return render(request, 'investor/issu.html')
 
 
+@login_required(login_url = 'login')
 def request_buy(request):
     if request.method == 'POST':
         email = request.POST['email']
@@ -46,6 +53,7 @@ def request_buy(request):
         return redirect(request.META['HTTP_REFERER'])
 
 
+@login_required(login_url = 'login')
 def request_withdraw(request):
     if request.method == 'POST':
         email = request.POST['email']
@@ -64,8 +72,9 @@ def request_withdraw(request):
             return redirect(request.META['HTTP_REFERER'])
     else:
         return redirect(request.META['HTTP_REFERER'])
+    
 
-
+@login_required(login_url = 'login')
 def request_issu(request):
     if request.method == 'POST':
         email = request.POST['email']
@@ -84,3 +93,29 @@ def request_issu(request):
             return redirect(request.META['HTTP_REFERER'])
     else:
         return redirect(request.META['HTTP_REFERER'])
+    
+    
+    
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('p_home')
+
+    else:
+        if request.method == 'POST' :
+           username = request.POST.get('username')
+           password = request.POST.get('password')
+           user = authenticate(request ,username = username ,password = password)
+
+           if user is not None:
+               login(request , user)
+               return redirect('p_home')
+
+           else:
+               messages.info(request, 'Username or Password is incorrect')
+        context ={}
+        return render(request, 'login.html', context)
+
+
+def logutPage(request):
+    logout(request)
+    return redirect('login')
